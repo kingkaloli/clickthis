09fff18e2daaec5e839b5f82fd057a1235ba9e4a
Kaloli <rvellark@gmail.com>
2019-04-10 19:09:19 -0600

    Updates

---
+++ precache-manifest.d3cba76aadc99203d5ae68fc3c142422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad0b71871cec816fe1db",
+    "url": "/clickthis/static/js/runtime~main.b6aa53be.js"
+  },
+  {
+    "revision": "dd8caafd73760ad895ef",
+    "url": "/clickthis/static/js/main.c8b90c60.chunk.js"
+  },
+  {
+    "revision": "984819353907d8b4d268",
+    "url": "/clickthis/static/js/2.9f68371a.chunk.js"
+  },
+  {
+    "revision": "dd8caafd73760ad895ef",
+    "url": "/clickthis/static/css/main.5ec7a243.chunk.css"
+  },
+  {
+    "revision": "4541cf1f7c46b7a466ac587087354d72",
+    "url": "/clickthis/index.html"
+  }
+];
